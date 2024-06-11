508bea39ce0766cc8904b4a58c336438cc3e5b73
Josue Gallardo <dotelias02@gmail.com>
2024-06-11 00:54:54 -0400

    rollback cambios de prueba, dejaron la embarrá

--- src/server.ts
+++ src/server.ts
@@ -25,15 +25,6 @@ const start = async () => {
     },
   });
 
-  //Probando conneccion mongo
-  // Configurar CORS
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
-
   app.use(
     "/api/trpc",
     trpcExpress.createExpressMiddleware({
