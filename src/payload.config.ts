693540ba490c0754c7c304f0f5884a6d6df43d9d
Josue Gallardo <dotelias02@gmail.com>
2024-06-13 12:24:32 -0400

    añadiendo colecciones a la db

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,34 +5,36 @@ import { webpackBundler } from "@payloadcms/bundler-webpack";
 import path from "path";
 import { Users } from "./collections/Users";
 import dotenv from "dotenv";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
 
 dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
+   path: path.resolve(__dirname, "../.env"),
 });
 
 export default buildConfig({
-  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
-  routes: {
-    admin: "/sell",
-  },
-  admin: {
-    user: "users",
-    bundler: webpackBundler(),
-    meta: {
-      titleSuffix: "- Hippo Marketplace",
-      favicon: "/favicon.ico",
-      ogImage: "/thumbnail.jpg",
-    },
-  },
-  rateLimit: {
-    max: 2000,
-  },
-  editor: slateEditor({}),
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URL!,
-  }),
-  typescript: {
-    outputFile: path.resolve(__dirname, "payload-types.ts"),
-  },
+   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+   collections: [Users, Media, Products],
+   routes: {
+      admin: "/sell",
+   },
+   admin: {
+      user: "users",
+      bundler: webpackBundler(),
+      meta: {
+         titleSuffix: "- Hippo Marketplace",
+         favicon: "/favicon.ico",
+         ogImage: "/thumbnail.jpg",
+      },
+   },
+   rateLimit: {
+      max: 2000,
+   },
+   editor: slateEditor({}),
+   db: mongooseAdapter({
+      url: process.env.MONGODB_URL!,
+   }),
+   typescript: {
+      outputFile: path.resolve(__dirname, "payload-types.ts"),
+   },
 });
